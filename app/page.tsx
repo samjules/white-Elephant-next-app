e6891e6080a757b02212161f0c4535a51ed85d27
samjules <samjulander@gmail.com>
2025-02-18 21:56:53 -0900

    added elemetns and pictures

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,33 @@
-"use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+import Welcome from "../app/components/welcome"
+import Addcustomer from "../app/components/addcustomer"
+import Sharegoogle from "../app/components/sharegooglereview"
+import Notifications from "../app/components/notifications"
+const APP = () => {
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+<div className="hero"> 
+
+  
+    < Welcome />
+    
+
+    <div>
+    < Addcustomer />
+    </div>
+      
+    <div>
+    < Sharegoogle />
+    </div>
+      
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
+      < Notifications />
       </div>
-    </main>
+      
+</div>
+  
+  
   );
-}
+    };
+
+export default APP;
